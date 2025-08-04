687054d8dabd6f3f898aef7619cd49ad6e4ac882
Tusehufu <77408011+Tusehufu@users.noreply.github.com>
2025-08-04 17:47:43 +0200

    Gruppering

--- src/components/DrinkList.vue
+++ src/components/DrinkList.vue
@@ -45,39 +45,42 @@
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-6 col-lg-4 mb-4" v-for="drink in drinks" :key="drink.id">
-                <div class="card h-100 shadow-sm">
-                    <div class="d-flex flex-row h-100">
-                        <!-- Textkolumn -->
-                        <div class="p-3 d-flex flex-column justify-content-between flex-grow-1">
-                            <div>
-                                <h5 class="card-title">{{ drink.name }}</h5>
-                                <p class="card-text">{{ drink.description }}</p>
+        <div v-for="(drinkList, spirit) in groupedDrinks" :key="spirit" class="mb-4">
+            <h4 class="text-primary mb-3">{{ spirit }}</h4>
+            <div class="row">
+                <div class="col-md-6 col-lg-4 mb-4" v-for="drink in drinkList" :key="drink.id">
+                    <div class="card h-100 shadow-sm">
+                        <div class="d-flex flex-row h-100">
+                            <!-- Textkolumn -->
+                            <div class="p-3 d-flex flex-column justify-content-between flex-grow-1">
+                                <div>
+                                    <h5 class="card-title">{{ drink.name }}</h5>
+                                    <p class="card-text">{{ drink.description }}</p>
+                                </div>
+                                <button class="btn btn-primary mt-3 align-self-start"
+                                        :disabled="drink.soldOut"
+                                        @click="orderDrink(drink)">
+                                    {{ drink.soldOut ? 'Slutsåld' : 'Beställ' }}
+                                </button>
+                                <!-- Admin-knapp -->
+                                <button v-if="isAdmin && !drink.soldOut"
+                                        class="btn btn-sm btn-outline-danger mt-2 align-self-start"
+                                        @click="markSoldOut(drink)">
+                                    Ta bort
+                                </button>
+                                <button v-if="isAdmin && drink.soldOut"
+                                        class="btn btn-sm btn-outline-success mt-2 align-self-start"
+                                        @click="restoreDrink(drink)">
+                                    Återställ
+                                </button>
                             </div>
-                            <button class="btn btn-primary mt-3 align-self-start"
-                                    :disabled="drink.soldOut"
-                                    @click="orderDrink(drink)">
-                                {{ drink.soldOut ? 'Slutsåld' : 'Beställ' }}
-                            </button>
-                            <!-- Admin-knapp -->
-                            <button v-if="isAdmin && !drink.soldOut"
-                                    class="btn btn-sm btn-outline-danger mt-2 align-self-start"
-                                    @click="markSoldOut(drink)">
-                                Ta bort
-                            </button>
-                            <button v-if="isAdmin && drink.soldOut"
-                                    class="btn btn-sm btn-outline-success mt-2 align-self-start"
-                                    @click="restoreDrink(drink)">
-                                Återställ
-                            </button>
-                        </div>
 
-                        <!-- Bildkolumn -->
-                        <img :src="drink.image"
-                             class="img-fluid rounded-end"
-                             alt="Bild på {{ drink.name }}"
-                             style="object-fit: cover; width: 120px; height: 100%;" />
+                            <!-- Bildkolumn -->
+                            <img :src="drink.image"
+                                 class="img-fluid rounded-end"
+                                 alt="Bild på {{ drink.name }}"
+                                 style="object-fit: cover; width: 120px; height: 100%;" />
+                        </div>
                     </div>
                 </div>
             </div>
@@ -87,7 +90,7 @@
 
 
 <script setup lang="ts">
-    import { ref, onMounted } from 'vue'
+    import { ref, onMounted, computed } from 'vue'
     import { supabase } from '../lib/supabase'
 
     type Drink = {
@@ -96,6 +99,8 @@
         description: string
         image: string
         soldOut?: boolean
+        spirit: String
+
     }
 
     const drinks = ref<Drink[]>([
@@ -104,84 +109,98 @@
             name: 'The Bicycle Thief',
             description: 'Gin, Campari, Grapefruktjuice, Citronjuice, Sockerlag, Sodavatten',
             image: '/images/the-bicycle-thief.jpg',
+            spirit: 'Gin',
         },
         {
             id: 2,
             name: 'Pornstar Martini',
             description: 'Vodka, Passionsfruktslikör, Sockerlag, Limejuice',
             image: '/images/pornstar-martini.jpg',
+            spirit: 'Vodka',
         },
         {
             id: 3,
             name: 'Gin Fizz',
             description: 'Gin, citronjuice, sockerlag, äggvita, sodavatten',
             image: '/images/gin-fizz.jpg',
+            spirit: 'Gin',
         },
         {
             id: 4,
             name: 'Spicy Margarita',
             description: 'Tequila, Triple sec, Limejuice, AgaveSirap',
             image: '/images/spicy-margarita.jpg',
+            spirit: 'Tequila',
         },
         {
             id: 5,
             name: 'Fizzypop',
             description: 'Vodka, Blå Curaçao, Monin BubblegumSirap, Citronjuice, 7up',
             image: '/images/fizzy-pop.jpg',
+            spirit: 'Vodka',
         },
         {
             id: 6,
             name: 'Midori Sour',
             description: 'Vodka, Midori, Citronjuice, Limejuice, Sodavatten',
             image: '/images/midori-sour.jpg',
+            spirit: 'Vodka',
         },
         {
             id: 7,
             name: 'Tom Collins',
             description: 'Gin, Citronjuice, Sockerlag, Sodavatten',
             image: '/images/tom-collins.jpg',
+            spirit: 'Gin',
         },
         {
             id: 8,
             name: 'Margarita',
             description: 'Tequila, Triple sec, Limejuice, Sockerlag',
             image: '/images/margarita.jpg',
+            spirit: 'Tequila',
         },
         {
             id: 9,
             name: 'Caipirinha',
             description: 'Cachaça, Lime, Socker',
             image: '/images/caipirinha.jpg',
+            spirit: 'Rom',
         },
         {
             id: 10,
             name: 'Kiwi Caipirinha',
             description: 'Cachaça, Lime, Socker, Kiwi',
             image: '/images/kiwi-caipirinha.jpg',
+            spirit: 'Rom',
         },
         {
             id: 11,
             name: 'Aperol Spritz',
             description: 'Aperol, Bubbel, Sodavatten',
             image: '/images/aperol-spritz.jpg',
+            spirit: 'Aperol',
         },
         {
             id: 12,
             name: 'Thai Basil',
             description: 'Gin, Citronjuice, Thaibasilikasockerlag, Kokosskum',
             image: '/images/thai-basil.jpg',
+            spirit: 'Gin',
         },
         {
             id: 13,
             name: 'Southside',
             description: 'Gin, Limejuice, Sockerlag, Mynta',
             image: '/images/south-side.jpg',
+            spirit: 'Gin',
         },
         {
             id: 14,
             name: 'Mojito',
             description: 'Rom, Limejuice, Sockerlag, Sodavatten, Mynta',
             image: '/images/mojito.jpg',
+            spirit: 'Rom',
         },
     ])
     const isAdmin = ref(false)
@@ -192,6 +211,18 @@
     const selectedDrink = ref<Drink | null>(null)
     const customerName = ref('')
     const orderConfirmed = ref(false)
+
+    const groupedDrinks = computed(() => {
+        const groups: Record<string, Drink[]> = {}
+        for (const drink of drinks.value) {
+            const category = drink.spirit
+            if (!groups[category]) {
+                groups[category] = []
+            }
+            groups[category].push(drink)
+        }
+        return groups
+    })
 
     onMounted(() => {
         isAdmin.value = localStorage.getItem('isAdmin') === 'true'
